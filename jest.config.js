8a536f83541cb171b38bb0f22967877fb919976c
Barna Burom <barna@codingsans.com>
2019-10-14 17:35:27 +0200

    feat(jest.config): update with text

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
+  verbose: true,
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!src/**/*.factory.ts'],
-  coverageReporters: ['html', 'json'],
+  coverageReporters: ['html', 'json', 'text'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
